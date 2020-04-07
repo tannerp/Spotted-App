b61f6f13d10ec3e055f8a8579745e38c55904f8b
Tanner Phan <tannerphan@gmail.com>
2020-04-07 07:23:50 -0700

    Project directory structuring

--- src/server.ts
+++ src/server.ts
@@ -1,65 +1,41 @@
-import { Request, Response } from 'express';
 import express from 'express';
+import { sequelize } from './sequelize';
+
+import { IndexRouter } from './controllers/v0/index.router';
+
 import bodyParser from 'body-parser';
-import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { config } from './config/config';
+import { V0MODELS } from './controllers/v0/model.index';
+
+const c = config.dev;
 
 (async () => {
+  await sequelize.addModels(V0MODELS);
+  await sequelize.sync();
 
-  // Init the Express application
   const app = express();
-
-  // Set the network port
-  const port = process.env.PORT || 8082;
-
-  // Use the body parser middleware for post requests
+  const port = process.env.PORT || 8080; // default port to listen
+  
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //CORS Should be restricted
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", c.url);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
-  /**************************************************************************** */
+  app.use('/api/v0/', IndexRouter)
 
-  //! END @TODO1
+  // Root URI call
+  app.get( "/", async ( req, res ) => {
+    res.send( "/api/v0/" );
+  } );
+  
 
-  app.get("/filteredimage", async (req: Request, res: Response) => {
-    const { image_url }: { image_url: string } = req.query;
-    if (!image_url) {
-      return res.status(401)
-        .send("Please include an image_url in the query parameters");
-    }
-    // const filteredImage: Promise = await filterImageFromURL(image_url);
-    
-    try{
-      const filteredImage = await filterImageFromURL(image_url);
-      return res.status(200).sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
-    } catch (e){
-      console.log("ERROR")
-      return res.status(422).send({error: e})
-    }
-});
-
-
-// Root Endpoint
-// Displays a simple message to the user
-app.get("/", async (req: Request, res: Response) => {
-  res.send("try URL GET /filteredimage?image_url={{}}")
-});
-
-
-// Start the Server
-app.listen(port, () => {
-  console.log(`server running http://localhost:${port}`);
-  console.log(`press CTRL+C to stop server`);
-});
-}) ();
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running ` + c.url );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
