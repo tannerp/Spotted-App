94f26c583b847e6b6e0c2099efe4ea0fdc7b6433
Tanner Phan <tannerphan@gmail.com>
2020-04-07 20:46:59 -0700

    mend

--- build-backend/controllers/v0/model.index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("./users/models/User");
-exports.V0MODELS = [User_1.User];
-//# sourceMappingURL=model.index.js.map
