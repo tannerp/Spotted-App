1b21fd57789dd9751bb6cb3daabfb743a12540e5
Yani L <yanili@spu.edu>
2020-05-19 18:28:49 -0700

    frame work for new post
    construct new post class and  post input field. add snackbar, working states are : post error, save post, post loading and loaded

--- frontend/lib/home/new_post.dart
+++ frontend/lib/home/new_post.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 // BloC
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotted/models/post.dart';
 import 'package:spotted/post/bloc.dart';
 
 // NOTE can start with creating the UI. We will wire it up later
 
-class NewPost extends StatelessWidget {
+/*class NewPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,7 @@ class NewPost extends StatelessWidget {
       if (state is PostLoaded) {
         
         return Scaffold(
-      appBar: AppBar(title: Text("Verify Email")),
+      appBar: AppBar(title: Text("New Post")),
       body: Container(
         margin: EdgeInsets.all(24),
         child: Form(
@@ -60,4 +61,95 @@ class NewPost extends StatelessWidget {
     });
 }
 
+}*/
+
+
+
+class NewPost extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Post"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical:16),
+        alignment: Alignment.center,
+        child: BlocListener <PostBloc, PostState> (
+          listener: (context,state) {
+            if (state is PostError) {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(state.message),
+                ),
+              );
+            }
+          },
+          child: BlocBuilder<PostBloc, PostState>(
+            builder: (context, state) {
+              if (state is PostEmpty){
+                return buildInitialInput();
+                //BlocProvider.of<PostBloc>(context).add(FetchPost());
+              } else if (state is PostLoading) {
+                return buildLoading();
+              } else if (state is PostLoaded) {
+                return buildLoading();
+              } else if (state is PostSaving) {
+                return buildSaving();
+              } else if (state is PostError) {
+                return buildInitialInput();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildInitialInput() {
+    return Center(
+      child: PostInputField(),
+      );
+  }
+  
+  Widget buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget buildLoaded() {
+    return Center();
+  }
+ 
+  Widget buildSaving() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+}
+
+// Todo, add one more textField
+class PostInputField extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        onSubmitted: (value) => submitPost(context, new Post(body: value, title: value)),
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: "What's on your mind?",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: Icon(Icons.save),
+        ),
+      ),
+    );
+  }
+
+  // Sumbit button method(add event to the Bloc)
+  void submitPost(BuildContext context, Post post) {
+    final bloc = BlocProvider.of<PostBloc>(context);
+    bloc.add(SavePost(post));
+  }
 }
