fd8edb11d2164b5084da7610b3b40bc5c50bf2b6
sherpalhakpa <sherpal@spu.edu>
2020-05-31 15:12:21 -0700

    User Model and Profile BloC updated - user model now holds information for major,year and housing information - two different bloc for myprofile and public profile

--- frontend/lib/home/MyProfilePage.dart
+++ frontend/lib/home/MyProfilePage.dart
@@ -1,60 +1,60 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:spotted/components/myProfileTile.dart';
-import 'package:spotted/profile/profile_bloc.dart';
-import 'package:spotted/profile/profile_event.dart';
+import 'package:spotted/profile/bloc.dart';
 import 'package:spotted/profile/profile_state.dart';
+import 'package:spotted/profile/profile_event.dart';
+import 'package:spotted/components/myProfileTile.dart';
+import 'package:spotted/models/user.dart';
 
-class MyProfilePage extends StatelessWidget {
+class ProfilePage extends StatelessWidget {
+  
+
   @override
   Widget build(BuildContext context) {
-    return BlocListener<ProfileBloc, ProfileState>(listener: (context, state) {
+    BlocProvider.of<ProfileBloc>(context).add(FetchProfile());
+
+    return BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
+      
       if (state is ProfileEmpty) {
         BlocProvider.of<ProfileBloc>(context).add(FetchProfile());
       }
 
       if (state is ProfileError) {
-        Scaffold.of(context).showSnackBar(
-          SnackBar(
-            content: Text('${state.message}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    }, 
-    child: BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {    
-
-      if (state is ProfileError) {
         return Center(
-          child: Text('Failed to fetch profile information'),
+          child: Text('failed to fetch profile'),
         );
       }
 
       if (state is ProfileLoaded) {
-        print("Profile information loaded");
-        print(state);
-        if (state.user == null) return Container();
-
-        return ProfilePageWidget(state.user.firstName, state.user.lastName);
+        //fetchprofile method should return User
+        return ProfilePageWidget(
+          state.user.firstName, 
+          state.user.lastName, 
+          "email",
+          state.user.major,
+          state.user.classStanding,
+          state.user.housing,
+          () => saveUser(context, new User(
+            token: state.user.token,
+            userID: state.user.userID,
+            firstName: state.user.firstName,
+            lastName: state.user.lastName,
+            major: state.user.major,
+            classStanding: state.user.classStanding,
+            housing: state.user.housing,
+          )),
+          );
       }
-
-      if (state is ProfileLoading) {
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-      if (state is ProfileError) {
-        Scaffold.of(context).showSnackBar(
-          SnackBar(
-            content: Text('${state.message}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-
-      print(state);
-      return Container();
-    }));
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    });
   }
+}
+
+void saveUser(BuildContext context, User user) {
+    final bloc = BlocProvider.of<ProfileBloc>(context);
+    bloc.add(SaveProfile(user));
 }
---
+++ frontend/lib/home/ViewUserProfilePage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:spotted/components/userProfileTile.dart';
+import 'package:spotted/profile/profile_bloc.dart';
+import 'package:spotted/profile/profile_event.dart';
+import 'package:spotted/profile/profile_state.dart';
+
+class MyProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ProfileBloc, ProfileState>(listener: (context, state) {
+      if (state is ProfileEmpty) {
+        BlocProvider.of<ProfileBloc>(context).add(FetchProfile());
+      }
+
+      if (state is ProfileError) {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('${state.message}'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    }, 
+    child: BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {    
+
+      if (state is ProfileError) {
+        return Center(
+          child: Text('Failed to fetch profile information'),
+        );
+      }
+
+      if (state is ProfileLoaded) {
+        print("Profile information loaded");
+        print(state);
+        if (state.user == null) return Container();
+
+        return UserProfilePageWidget(
+          state.user.firstName, 
+          state.user.lastName, 
+          "email",
+          state.user.major,
+          state.user.classStanding,
+          state.user.housing,
+          );
+      }
+
+      if (state is ProfileLoading) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      if (state is ProfileError) {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('${state.message}'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+
+      print(state);
+      return Container();
+    }));
+  }
+}
